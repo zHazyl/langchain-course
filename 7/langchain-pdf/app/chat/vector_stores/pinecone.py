4f44de5870208d5be37a51e9aee65d6f1f6afdf7
zHazyl <tnh210302@gmail.com>
2024-03-22 05:02:48 +0700

    redis

--- 7/langchain-pdf/app/chat/vector_stores/pinecone.py
+++ 7/langchain-pdf/app/chat/vector_stores/pinecone.py
@@ -4,21 +4,16 @@ from langchain.vectorstores import Pinecone
 from app.chat.embeddings.openai import embeddings
 from langchain.vectorstores.chroma import Chroma
  
-# pinecone.init(
-#     api_key=os.getenv("PINECONE_API_KEY"),
-#     environment=os.getenv("PINECONE_ENV_NAME")
-# )
- 
-# vector_store = Pinecone.from_existing_index(
-#     os.getenv("PINECONE_INDEX_NAME"), embeddings
-# )
-
 pinecone.init(
-    api_key='2f1c9240-3c15-4d50-b75e-a252d5769c3a',
-    environment='gcp-starter'
+    api_key=os.getenv("PINECONE_API_KEY"),
+    environment=os.getenv("PINECONE_ENV_NAME")
+)
+ 
+vector_store = Pinecone.from_existing_index(
+    os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
 
-vector_store = Chroma(
-    persist_directory="emb",
-    embedding_function=embeddings
-)
+# vector_store = Chroma(
+#     persist_directory="emb",
+#     embedding_function=embeddings
+# )
